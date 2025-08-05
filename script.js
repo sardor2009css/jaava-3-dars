d1e3e2a8d36135851d6913d979ef60068a31b71f
sardor2009css <sardortoshtemirov621@gmail.com>
2025-08-05 13:33:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let random = alert(`Sizda xozir random solnal chiqishni boshlaydi`)
+console.log(random);
+
+let randomson = Math.floor(Math.random () * 10) +1;
+alert(`sizda chiqqan randoom son ${randomson}`);
+
+let kopaytirish = randomson * 2;
+alert(`Randoom soning 2ga kopaytmasi ${kopaytirish}`);
+
+let boluvshi = randomson / 2;
+alert(`Randoom soning 2ga bolingani ${boluvshi}`)
+
+alert(`Random orqali chiqqan son ${randomson} Edi va biz uni 2ga kopaytirdig va javobi ${kopaytirish} Chiqti\nRandom sonni 2 ga bolganimizsda esa ${boluvshi} Chiqdi` )
+
